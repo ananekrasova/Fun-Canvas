e2a35c11fda177cc76e6031a4c47137685b2b024
Anna <averbah-ann@mail.ru>
2021-07-18 17:07:36 +0400

    test

--- main.js
+++ main.js
@@ -45,4 +45,6 @@ canvas.addEventListener('mousedown', (e) => {
 canvas.addEventListener('mousemove', draw)
 
 canvas.addEventListener('mouseup', () => isDrawing = false)
-canvas.addEventListener('mouseout', () => isDrawing = false)  
+canvas.addEventListener('mouseout', () => isDrawing = false)
+
+// test
